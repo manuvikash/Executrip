7f66abf43a7bb900c8b5458cc575a99e2f4889f4
Manuvikash Saravanakumar <manuvikashs@gmail.com>
2023-03-04 12:27:01 +0530

    Made basic backend methods and directory structure

--- server/controllers/Controller.js
+++ server/controllers/Controller.js
@@ -1,16 +1,21 @@
-const getReq = (req, res) => {
+const getReq = async (req, res) => {
   res.status(200).json({ express: "get request" });
 };
 
-const postReq = (req, res) => {
-  res.status(200).json({ express: "post request" });
+const postReq = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400).json({ message: "text is required" });
+  } else {
+    console.log(req.body);
+    res.status(200).json({ express: `post request with body ${req.body}` });
+  }
 };
 
-const putReq = (req, res) => {
+const putReq = async (req, res) => {
   res.status(200).json({ express: `Put req with id = ${req.params.id}` });
 };
 
-const deleteReq = (req, res) => {
+const deleteReq = async (req, res) => {
   res.status(200).json({ express: `Delete req with id = ${req.params.id}` });
 };
 
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
-app.use("/api/hello", require("./routes/Routes"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", require("./routes/Routes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
