18f8d4a1757356ee2c107d4bc7cb4e41c0749db7
manuvikash <manuvikashs@gmail.com>
2023-03-07 15:21:36 +0530

    Fixed signup redirection

--- server/server.js
+++ server/server.js
@@ -3,26 +3,27 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const routes = require("./routes/Routes");
 const cors = require("cors");
-const expressSession = require("express-session")
+const expressSession = require("express-session");
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(expressSession({
-  name: "testSession",
-  resave: false,
-  saveUninitialized:false,
-  secret: process.env.SESSION_SECRET,
-  cookie:
-  {
-     maxAge: 1000 * 60 * 60 * 24,
-  },
-}))
+app.use(
+  expressSession({
+    name: "testSession",
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 const mongoString = process.env.DATABASE_URL;
-console.log(mongoString)
+console.log(mongoString);
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
